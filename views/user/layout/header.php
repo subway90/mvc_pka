fd9ec5542fe686d5088259965e27f8afc082215e
subway90 <nguyenhieu3105@gmail.com>
2025-01-14 22:52:40 +0700

    update view header

--- views/user/layout/header.php
+++ views/user/layout/header.php
@@ -2,76 +2,102 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title><?= (isset($title)) ? $title : WEB_NAME ?></title>
-  <meta name="description" content="">
-  <meta name="keywords" content="">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title><?= (isset($title)) ? $title : WEB_NAME ?></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 
-  <!-- Favicons -->
-  <link href="https://cntt.epu.edu.vn/Images/favicon.png" rel="icon">
+    <!-- Favicons -->
+    <link href="https://cntt.epu.edu.vn/Images/favicon.png" rel="icon">
 
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com" rel="preconnect">
-  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="<?=URL?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?=URL?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?=URL?>assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="<?=URL?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="<?=URL?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="<?= URL ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= URL ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?= URL ?>assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="<?= URL ?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="<?= URL ?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Main CSS File -->
-  <link href="<?=URL?>assets/css/main.css" rel="stylesheet">
+    <!-- Main CSS File -->
+    <link href="<?= URL ?>assets/css/main.css" rel="stylesheet">
 
 </head>
 
 <?=
-// dùng toast
-toast_show();
+        // dùng toast
+    toast_show();
 ?>
 
 <body class="contact-page">
 
-  <header id="header" class="header d-flex align-items-center sticky-top">
-    <div class="container position-relative d-flex align-items-center">
+    <header id="header" class="header d-flex align-items-center sticky-top">
+        <div class="container position-relative d-flex align-items-center">
 
-      <a href="index.html" class="logo d-flex align-items-center me-auto">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO-9Cx2kvUrVEbpBdDspON-K894ASoV4BFLQ&s" alt="">
-        <!-- <h1 class="sitename">Company</h1><span>.</span> -->
-      </a>
+            <a href="index.html" class="logo d-flex align-items-center me-auto">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO-9Cx2kvUrVEbpBdDspON-K894ASoV4BFLQ&s"
+                    alt="">
+                <!-- <h1 class="sitename">Company</h1><span>.</span> -->
+            </a>
 
-      <nav id="navmenu" class="navmenu">
-        <ul>
-          <li><a href="<?=URL?>trang-chu" class="<?=($page=='home')?'active':''?>">Trang chủ</a></li>
-          <li><a href="<?=URL?>dang-nhap" class="<?=($page=='login')?'active':''?>">Đăng nhập</a></li>
-          <li><a href="<?=URL?>tin-tuc" class="<?=($page=='blog')?'active':''?>">Tin tức</a></li>
-          <li class="dropdown"><a href="about.html"><span>About</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-              <li><a href="team.html">Team</a></li>
-              <li><a href="testimonials.html">Testimonials</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <nav id="navmenu" class="navmenu">
                 <ul>
-                  <li><a href="#">Deep Dropdown 1</a></li>
-                  <li><a href="#">Deep Dropdown 2</a></li>
-                  <li><a href="#">Deep Dropdown 3</a></li>
-                  <li><a href="#">Deep Dropdown 4</a></li>
-                  <li><a href="#">Deep Dropdown 5</a></li>
+                    <li><a href="<?= URL ?>trang-chu" class="<?= ($page == 'home') ? 'active' : '' ?>">Trang chủ</a></li>
+                    <li><a href="<?= URL ?>tin-tuc" class="<?= ($page == 'blog') ? 'active' : '' ?>">Tin tức</a></li>
+                    <li class="dropdown"><a href="about.html"><span>About</span> <i
+                                class="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                            <li><a href="team.html">Team</a></li>
+                            <li><a href="testimonials.html">Testimonials</a></li>
+                            <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i
+                                        class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                <ul>
+                                    <li><a href="#">Deep Dropdown 1</a></li>
+                                    <li><a href="#">Deep Dropdown 2</a></li>
+                                    <li><a href="#">Deep Dropdown 3</a></li>
+                                    <li><a href="#">Deep Dropdown 4</a></li>
+                                    <li><a href="#">Deep Dropdown 5</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <?php if ($is_user) { ?>
+                        <li class="dropdown">
+                            <a href="#">
+                                <span>
+                                    <img class="me-2 rounded-circle" width="30" src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg" alt="">
+                                </span>
+                                <span><?=$_SESSION['user']['full_name']?></span>
+                                 <i class="bi bi-chevron-down toggle-dropdown"></i>
+                            </a>
+                                <ul>
+                                    <?php if($is_admin) { ?>
+                                    <li><a href="<?=URL_ADMIN?>">Trang quản trị</a></li>
+                                    <?php }?>
+                                    <li><a href="#">Trang cá nhân</a></li>
+                                    <li><a href="#">Đồ án</a></li>
+                                    <hr>
+                                    <li><a href="<?=URL?>dang-xuat">Đăng xuất</a></li>
+                                </ul>
+                            </li>
+                    <?php } else { ?>
+                        <li><a href="<?= URL ?>dang-nhap" class="<?= ($page == 'login') ? 'active' : '' ?>">Tài khoản</a></li>
+                    <?php } ?>
                 </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-      </nav>
+                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+            </nav>
 
-      <div class="header-social-links">
-        <a href="#" class="linkedin"><i class="bi bi-user"></i></a>
-      </div>
+            <div class="header-social-links">
+                <a href="#" class="linkedin"><i class="bi bi-user"></i></a>
+            </div>
 
-    </div>
-  </header>
+        </div>
+    </header>
 
-  <main class="main">
+    <main class="main">
