3335c03ab2481a9aaa5eabea9fc48b1a35d6c445
subway90 <nguyenhieu3105@gmail.com>
2025-01-15 20:37:31 +0700

    fix admin site not active

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(isset($_GET['act']) && $_GET['act']) {
     // kiểm tra có phải action của admin
     if($_action === 'admin') {
         // Kiểm tra có phải là admin hay không
-        if(author('admin')) return route('dang-nhap');
+        if(!author('admin')) return route('dang-nhap');
         // Cắt phần tử đầu tiên, tức xoá phần tử chứa 'admin'
         $_arrayURL = array_slice($_arrayURL, 1);
         // Kiểm tra request có rỗng không, để lấy action
