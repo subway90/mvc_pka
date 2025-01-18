e846e9c691b6ae1b5022cda21d3d36da204dfefd
subway90 <nguyenhieu3105@gmail.com>
2025-01-18 09:42:16 +0700

    update config example

--- example.config.php
+++ example.config.php
@@ -1,13 +1,20 @@
 <?php
 
-const WEB_NAME = 'Bống Béo Bread';
+const WEB_NAME = 'Khoa CNTT - EPU';
 
-const URL = 'http://localhost:8080/';
+const URL = 'https://subway90.vn/';
+const URL_ADMIN = URL.'admin/';
 
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
 const DB_PASS = '';
-const DB_NAME = 'mvc_bongbeobread';
+const DB_NAME = 'mvc_pka';
 
-const VNPAY_TMNCODE = '';
-const VNPAY_HASHKEY = '';
+const TOAST_TIME = 3000;
+
+const DEFAULT_IMG_USER = 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg';
+const DEFAULT_IMG_BLOG = 'https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg';
+
+const PATH_FILE_PROJECT = "assets/file/project/";
+const PATH_FILE_AVATAR = "assets/file/avatar/";
+const PATH_FILE_BLOG = "assets/file/blog/";
